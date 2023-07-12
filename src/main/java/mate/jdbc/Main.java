6554d34216e9dda145c2d1e224422fe0b5105ec9
Slava-png <pylyp.sviatoslav@gmail.com>
2023-07-12 16:15:26 +0200

    added dao and service layers for Driver and Manufacturer

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,23 @@
 package mate.jdbc;
 
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.service.DriverService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
+
+        Driver driver = new Driver(1L, "Alex", "0000");
+        driverService.create(driver);
+
+        driverService.update(new Driver(8L, "Harry", "2983"));
+        System.out.println(driverService.delete(8881L));
+        driverService.getAll().forEach(System.out::println);
+
+        driverService.get(2L);
+        driverService.get(10L);
     }
 }
